0399b7d5828ec3a1d49cd28890648984787132fd
Benjamin <ge93leh@tum.de>
2021-11-27 23:35:56 +0100

    Manuelle Board-Eingabe

--- board.py
+++ board.py
@@ -1,6 +1,9 @@
 import numpy as np
+import time
 
-def init_board():
+
+def init_board(manual_input):
+
     '''
     11 |  12  | 13
     --------------
@@ -8,20 +11,54 @@ def init_board():
     --------------
     31 |  32  | 33
     '''
-    cell_11 = np.matrix([[1,1,1],[1,6,1],[1,1,1]])
-    cell_12 = np.matrix([[2,2,2],[2,2,2],[2,2,2]])
-    cell_13 = np.matrix([[3,3,3],[3,3,3],[3,3,3]])
-    cell_21 = np.matrix([[4,4,4],[4,4,4],[4,4,4]])
-    cell_22 = np.matrix([[5,5,5],[5,5,5],[5,5,5]])
-    cell_23 = np.matrix([[6,6,6],[6,6,6],[6,6,6]])
-    cell_31 = np.matrix([[7,7,7],[7,7,7],[7,7,7]])
-    cell_32 = np.matrix([[8,8,8],[8,8,8],[8,8,8]])
-    cell_33 = np.matrix([[9,9,9],[9,9,9],[9,9,9]])
 
-    board = [[cell_11, cell_12, cell_13],[cell_21,cell_22, cell_23], [cell_31, cell_32, cell_33]]
+    if manual_input:
+        #initialisieren
+        cell_11 = np.ones((3,3)) * -1
+        cell_12 = np.ones((3,3)) * -1
+        cell_13 = np.ones((3,3)) * -1
+        cell_21 = np.ones((3,3)) * -1
+        cell_22 = np.ones((3,3)) * -1
+        cell_23 = np.ones((3,3)) * -1
+        cell_31 = np.ones((3,3)) * -1
+        cell_32 = np.ones((3,3)) * -1
+        cell_33 = np.ones((3,3)) * -1
+        board = [[cell_11, cell_12, cell_13],[cell_21,cell_22, cell_23], [cell_31, cell_32, cell_33]]
+        board_names = [['cell_11', 'cell_12', 'cell_13'],['cell_21','cell_22', 'cell_23'], ['cell_31', 'cell_32', 'cell_33']]
+
+
+        print('----------------------------------')
+        print('Manuelle Eingabe von Zahlen: \n')
+        for i in range(0,3): #globale Zeilen des Boards
+            row_global = i
+            for j in range(0,3): #globale Spalten des Boards
+                coloumb_global = j
+                cell = board[row_global][coloumb_global]
+                cell_name = board_names[row_global][coloumb_global]
+                print('Eingabe von Zelle ', cell_name,':')
+                for k in range(0,3): #Zeilen der Zelle
+                    row_cell = k
+                    for l in range(0,3): #Spalten der Zelle
+                        coloumb_cell = l
+                        print('Zeile:', row_cell+1, ' Spalte:', coloumb_cell+1, ' ', end='')
+                        temp = input()
+                        cell[row_cell][coloumb_cell] = int(temp) 
+
+
+    else:
+        cell_11 = np.matrix([[1,1,1],[1,6,1],[1,1,1]])
+        cell_12 = np.matrix([[2,2,2],[2,2,2],[2,2,2]])
+        cell_13 = np.matrix([[3,3,3],[3,3,3],[3,3,3]])
+        cell_21 = np.matrix([[4,4,4],[4,4,4],[4,4,4]])
+        cell_22 = np.matrix([[5,5,5],[5,5,5],[5,5,5]])
+        cell_23 = np.matrix([[6,6,6],[6,6,6],[6,6,6]])
+        cell_31 = np.matrix([[7,7,7],[7,7,7],[7,7,7]])
+        cell_32 = np.matrix([[8,8,8],[8,8,8],[8,8,8]])
+        cell_33 = np.matrix([[9,9,9],[9,9,9],[9,9,9]])
+        board = [[cell_11, cell_12, cell_13],[cell_21,cell_22, cell_23], [cell_31, cell_32, cell_33]]
+    
+    time.sleep(1)
     return board
-
-
 
 
 
@@ -48,5 +85,9 @@ def ausgabe(board):
                 print('\n| ', end = '') #Zeilenumbruch und Anfang neuer Zeile
 
 
-board = init_board()
+manual_input = 1
+board = init_board(manual_input)
 ausgabe(board)
+
+
+time.sleep(3)
