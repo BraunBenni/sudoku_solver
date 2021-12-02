ef812777278ae03d2f7d9ca4fd86fd4024dfdc18
Benjamin <ge93leh@tum.de>
2021-12-02 15:51:23 +0100

    solve with backtracking alogrithm

--- backtracking_solver.py
+++ backtracking_solver.py
@@ -1,3 +1,6 @@
+import numpy as np
+import time
+
 def checkNumber(board, row, coloumb, number):
     '''
     Function checks whether new number is valid:
@@ -6,12 +9,12 @@ def checkNumber(board, row, coloumb, number):
 
     #check row:
     for i in range(0,8):
-        if board[row][i] == number:
+        if board[int(row)][i] == number:
             passed = 0
 
     #check coloumb:
     for i in range(0,8):
-        if board[i][coloumb] == number:
+        if board[i][int(coloumb)] == number:
             passed = 0
     
     #check cell:
@@ -38,7 +41,11 @@ def checkNumber(board, row, coloumb, number):
 def findNextEmpty(board, current_row, current_coloumb):
     '''
     Function to find the next empty element: rows first
+
+    returns -1 & -1, if it can't find an empty element
     '''
+    empty_row = -1 #in case sudoku is alreadey solved and we can't find an empty element
+    empty_coloumb = -1 #in case sudoku is alreadey solved and we can't find an empty element
     found_empty = 0
     for i in range(current_row, 9):
         if i == current_row: #in the current row, we don't need to iterate through all columbs
@@ -48,7 +55,7 @@ def findNextEmpty(board, current_row, current_coloumb):
                     empty_coloumb = j
                     found_empty = 1 #bool to exit two for-loops
                     break
-        else:
+        else: #all other rows
             for j in range(0, 9):
                 if board[i][j] == 0:
                     empty_row = i
@@ -62,14 +69,59 @@ def findNextEmpty(board, current_row, current_coloumb):
 
 
 board = [
-    [7,8,0,4,0,0,1,2,0], #0
-    [6,0,0,0,7,5,0,0,9], #1
-    [0,0,0,6,0,1,0,7,8], #2
-    [0,0,7,0,4,0,2,6,0], #3
-    [0,0,1,0,5,0,9,3,0], #4
-    [9,0,4,0,6,0,0,0,5], #5
-    [0,7,0,3,0,0,0,1,2], #6
-    [1,2,0,0,0,7,4,0,0], #7
-    [0,4,9,2,0,6,0,0,7]  #8
+    #0 1 2 3 4 5 6 7 8
+    [0,5,9,0,0,4,7,6,0], #0
+    [0,8,0,5,7,0,3,0,0], #1
+    [0,4,3,0,0,0,0,5,0], #2
+    [1,0,0,0,4,0,6,0,0], #3
+    [0,7,0,0,6,0,0,9,0], #4
+    [0,0,2,0,8,0,0,0,7], #5
+    [0,9,0,0,0,0,1,2,0], #6
+    [0,0,7,0,2,6,0,3,0], #7
+    [0,2,6,4,0,0,9,7,0]  #8
 ]
 
+start_time = time.time()
+
+row = 0 #starting row
+coloumb = 0 #starting coloumb
+solved = 0
+index_array = np.zeros([2,81]) #array to save edited elements (row and coloumb indices)
+step = 0
+while solved != 1:
+    row, coloumb = findNextEmpty(board, int(row), int(coloumb)) #seraching new empty element
+    print('row = ', row, '\ncoloumb = ', coloumb, '\n')
+    if row == -1: #checks whether soduko is already solved (findeNextEmpty returns -1 & -1, if it can't find an empty element)
+        solved = 1
+        break
+
+    number = 1
+    element_passed = 0
+    while element_passed != 1:
+        element_passed = checkNumber(board, row, coloumb, number)
+        if element_passed: #break if element passes test (and save row and coloumb indices)
+            board[int(row)][int(coloumb)] = number
+            index_array[0][step] = row 
+            index_array[1][step] = coloumb
+            step += 1
+            break
+        elif number in range(1,9): #element can still increase --> max: number = 8
+            number += 1
+        else: #reversing as many steps as necessary
+            reverse = 0
+            while reverse != 1:
+                step -= 1
+                #loading row and coloumb indices of previous step
+                row = index_array[0][step] 
+                coloumb = index_array[1][step]
+                number = board[int(row)][int(coloumb)] #loading number of previous step
+                board[int(row)][int(coloumb)] = 0 #deleting previous step --> again an empty element
+                #if possible, increasing number of previous step --> otherwise keep reversing
+                if number in range(1,9):
+                    reverse = 1 #found step we can reverse to
+                    number += 1
+                   
+            
+end_time = time.time()
+
+print("\n\n     --- execution time: %.7s seconds --- \n" % (end_time-start_time))
